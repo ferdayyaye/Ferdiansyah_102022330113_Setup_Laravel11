85d69d84063c2fc317aafc6c6fd8ae32c813c49d
Ferdiansyah Adi Saputra <ferdiasyh.a.s187@gmail.com>
2025-04-22 01:38:11 +0700

    Revert "Proyek Laravel 11 dengan Autentikasi"
    This reverts commit 865c97fcbbffec5d0109e8162adbde8c08560b70.

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/books');
 });
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'authenticating']);
-Route::get('/logout', [AuthController::class, 'logout']);
-Route::get('/forgot-password', function () {
-    return 'Halaman reset password (buat nanti jika perlu)';
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::get('/books', [BookController::class, 'index'])->name('books');
-    Route::get('/users', [UserController::class, 'index']);
-});
+Route::resource('books', BookController::class);
